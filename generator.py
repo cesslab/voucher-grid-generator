617ca12e5127ee3baa40726695d21d53719edf4a
aaruff <anwarruff@gmail.com>
2020-03-22 17:28:51 -0400

    fixed grid display and added text rendering

--- generator.py
+++ generator.py
@@ -11,8 +11,6 @@ class PDFGridGenerator():
 
     def __init__(self):
         self.pdf = FPDF()
-
-    def generate(self, filename):
         self.pdf.add_page()
         self.pdf.set_auto_page_break(False)
         self.pdf.set_margins(0, 0)
@@ -20,36 +18,52 @@ class PDFGridGenerator():
         self.pdf.set_draw_color(191, 187, 187)
         self.pdf.set_line_width(0.35)
 
+    def save(self, filename):
+        self.pdf.output(filename)
+
+    def generate_grid(self):
+        # Fonts must be set before writing text
+        self.pdf.set_font('Arial', 'B', 8)
+        self.pdf.set_text_color(0, 0, 0)
+
         self.generate_vertical_lines(5)
         self.generate_horizontal_lines(5)
 
-        # Fonts must be set before writing text
-        self.pdf.set_font('Arial', 'I', 8)
-        self.pdf.set_text_color(0, 0, 0)
-
-        self.generate_left_vertical_coordinate_numbers(5)
-        self.generate_bottom_horizontal_coordinate_numbers(5)
-
-        self.pdf.output(filename)
+        self.left_numbers(5)
+        self.top_numbers(5)
     
     def generate_vertical_lines(self, spacing):
-        for i in range(0, math.floor(self.pdf.w), spacing):
-            self.pdf.line(i, 0, i, self.pdf.h)
+        lines = 42 
+        for i in range(1, lines):
+            x = i*5
+            self.pdf.line(x, 5, x, 290)
     
     def generate_horizontal_lines(self, spacing):
-        for j in range(0, math.floor(self.pdf.h), spacing):
-            self.pdf.line(0, j, self.pdf.w, j)
+        lines = 59 
+        for j in range(1, lines):
+            y = j*5
+            self.pdf.line(5, y, 205, y)
 
-    def generate_left_vertical_coordinate_numbers(self, spacing):
-        for i in range(5, math.floor(self.pdf.h), spacing):
-            self.pdf.set_xy(1, i)
+    def left_numbers(self, spacing):
+        lines = 58
+        for i in range(0, lines):
+            self.pdf.set_xy(0, i*5 + 3)
             self.pdf.write(5, str(i))
 
-    def generate_bottom_horizontal_coordinate_numbers(self, spacing):
-        for i in range(5, math.floor(self.pdf.w), spacing):
-            self.pdf.set_xy(i, 1)
+    def top_numbers(self, spacing):
+        lines = 41
+        for i in range(0, lines):
+            self.pdf.set_xy(i*5 + 3, 1)
             self.pdf.write(5, str(i))
+
+    def text(self, x, y, txt, font_size=13):
+        self.pdf.set_font('Arial', '', font_size)
+        self.pdf.set_text_color(0, 0, 0)
+        self.pdf.set_xy(x*5+4, y*5+5)
+        self.pdf.write(5, txt)
 
 if __name__ == '__main__':
     grid_generator = PDFGridGenerator()
-    grid_generator.generate('grid.pdf')
+    grid_generator.generate_grid()
+    grid_generator.text(8, 16, "$12.50")
+    grid_generator.save('grid.pdf')
